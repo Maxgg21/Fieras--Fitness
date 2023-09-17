b1eef654470be9a0182d10749aedf7a28a23cd90
Maxgg21 <lionel.gegena@gmail.com>
2023-09-17 20:17:55 -0300

    Aplicando boostrap para maquetar, modularizando los componentes del header y footer

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.set("views", "./src/views");
+
+const indexRouter = require('./routes');  
+
+
+app.use('/', indexRouter)
+
+
+app.listen(PORT, () => console.log(`Express listening PORT: ${PORT}`))
---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    index: (req, res) => {
+        res.render('index')
+    },
+    detailPlan: (req, res) => {
+        res.render('detailPlan')
+    }
+}
