eb9192a9013138757c4096f04180934e6d67d6c6
Maxgg21 <lionel.gegena@gmail.com>
2023-09-23 22:42:59 -0300

    Agregando estilos de colores al header y banner

--- public/js/headerNav.js
+++ public/js/headerNav.js
@@ -1,6 +1,22 @@
-const navBar = document.getElementById('#navBar');
+let navBar = document.getElementById("buttonNavBar");
 
-const elem = document.getElementById("navBar");
+let mainContainer = document.getElementById('mainContainer');
 
+let subNavBar = document.getElementById('subNavBar');
 
-console.log(elem)
+navBar.addEventListener('click', function() {
+    mainContainer.classList.toggle('mainContainerMax')
+})
+
+subNavBar.addEventListener('click', function(){
+    mainContainer.classList.toggle('subMainContainerMax')
+})
+
+//scroll 
+window.addEventListener("scroll", () => {
+    if(window.scrollY >= 20){
+       document.querySelector(".navBarSticky").classList.add("header-opacity")
+   }else {
+       document.querySelector(".navBarSticky").classList.remove("header-opacity")
+   }
+})
