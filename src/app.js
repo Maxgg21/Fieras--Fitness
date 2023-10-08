d6744974f69d10c2d3b2b00bbb52a3b04dabb0e5
Maxgg21 <lionel.gegena@gmail.com>
2023-10-07 23:57:44 -0300

    Subiendo comando en app para que puedan ver los videos

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.static('public'));
+app.use('/videos', express.static('/public/videos'));
 app.set('view engine', 'ejs')
 app.set("views", "./src/views");
+
 
 const indexRouter = require('./routes');  
 
